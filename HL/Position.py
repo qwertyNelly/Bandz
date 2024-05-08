93e5b55dbe7ac2b1182c34b531bb1217c8bde09c
Ben Nelligan <36170767+qwertyNelly@users.noreply.github.com>
2024-05-08 02:39:43 -0400

    Update Position.py

--- HL/Position.py
+++ HL/Position.py
@@ -11,13 +11,25 @@ from hyperliquid.utils.types import (
     L2Book,
     TradesSubscription,
 )
+from HL.logging import LOGGING_CONFIG
 import os
 import logging as lg
 import logging.config as lgc
 
-from bndni.HL.errors.PositionExceptions import WalletNotFoundError
+from HL.errors.PositionExceptions import WalletNotFoundError
 
+
+from logging import getLogger as gl, StreamHandler as sh
+import logging.config as lgc
+
+
+lgc.dictConfig(LOGGING_CONFIG)
+
+log = lg.getLogger(__name__)
+log.debug(f'Initialized Logger for {__name__}')
+log.debug(f'Initializing Hyperliquid Connection to {mainnet}')
 hl = Info(mainnet)
+log.debug(f'Connected to Hyperliquid @ {mainnet} with URL {hl.base_url}')
 
 
 class position:
@@ -50,35 +62,52 @@ class position:
     coin: str = None
 
     leverage: int = 0
-    leverage_type: str = None  # Cross or Iso
+    leverage_type: str = 'ISO'  # Default to ISO
 
     margin_used: float = 0.0
+    max_leverage : int = 0
+    position_dict : dict = {}
+
 
     # Only Iso
-    raw_usd: str
+    raw_usd: str = ""
 
-    def __init__(self, wallet: str = None) -> None:
+    def __init__(self, position_data : dict,  wallet: str = None):
         self.wallet = wallet
+        self.position_dict= position_data
+
+
 
     @property
+    def leverage_type(self):
+        """
+        Probably not needed
+        """
+        self.leverage_type = 'ISO'
+        pass
+
+    
+    @property
     def wallet(self):
-        """_summary_
+        """
+        String of the wallet
 
         Raises:
             WalletNotFoundError: _description_
         """
         if self.wallet is None:
             self.wallet = os.environ.get("WALLET")
+            return os.environ.get("WALLET")
         # TODO: Add better exception handling
         else:
             raise WalletNotFoundError("Wallet not configured")
 
     @classmethod
     def init_position(cls, position: dict):
-        """_summary_
-
+        """
+        Run this First
         Args:
-            position (dict): _description_
+            position (dict): the shit coming from the api
         """
         # Time spent (Wasted) : 34 minutes
         # hate = ['szi', 'entryPx', 'liquidationPx', 'positionValue', 'marginUsed']
@@ -97,22 +126,45 @@ class position:
             cls.funding_since_open = position["position"]["coin"]["cumFunding"][
                 "sinceOpen"
             ]
+            # Leverage 
+            cls.leverage = position["position"]["coin"]["leverage"][
+                "valuue"
+            ]
+            cls.leverage_type = position["position"]["coin"]["leverage"][
+                "type"
+            ]
+            cls.max_leverage = position["position"]["coin"]["maxLeverage"]
             # Value
             cls.entry_price = position["position"]["coin"]["entryPx"]
             cls.liquidation_price = position["position"]["coin"]["liquidationPx"]
             cls.position_value = position["position"]["coin"]["positionValue"]
             cls.margin_used = position["position"]["coin"]["marginUsed"]
+            cls.return_on_equity = position["position"]["coin"]["returnOnEquity"]
+            cls.leverage_type = position["position"]["coin"]["type"]
+            # ISO Margin
+            if cls.leverage_type.__eq__('isolated'):
+                cls.raw_usd = position["position"]["coin"]["leverage"]['rawUsd']
+                cls.max_leverage = position["position"]["coin"]["leverage"]['rawUsd']
+            return cls
+
 
     def get_positions(self) -> dict:
         """{'assetPositions':
             [
             {'position':
-                    {'coin': 'ETH', 'cumFunding': {'allTime': '27.984481', 'sinceChange': '0.030138', 'sinceOpen': '0.030138'}, 'entryPx': '3141.3', 'leverage': {'type': 'cross', 'value': 8}, 'liquidationPx': '2668.7911259', 'marginUsed': '150.205673', 'maxLeverage': 50, 'positionValue': '1201.64539', 'returnOnEquity': '-0.02852322', 'szi': '0.3839', 'unrealizedPnl': '-4.29968'}, 'type': 'oneWay'},
+                    {'coin': 'ETH', 'cumFunding': {'allTime': '27.984481', 'sinceChange': '0.030138', 'sinceOpen': '0.030138'}, 'entryPx': '3141.3', 
+                    'leverage': {'type': 'cross', 'value': 8}, 
+                    'liquidationPx': '2668.7911259', 'marginUsed': '150.205673', 'maxLeverage': 50, 
+                    'positionValue': '1201.64539', 'returnOnEquity': '-0.02852322', 'szi': '0.3839', 
+                    'unrealizedPnl': '-4.29968'}, 'type': 'oneWay'},
             {'position':
                     {'coin': 'TIA', 'cumFunding': {'allTime': '-0.166859', 'sinceChange': '0.076194', 'sinceOpen': '0.076194'}, 'entryPx': '10.217', 'leverage': {'type': 'cross', 'value': 20}, 'liquidationPx': '9.51093214', 'marginUsed': '156.4105', 'maxLeverage': 20, 'positionValue': '3128.21', 'returnOnEquity': '-0.24664774', 'szi': '310.0', 'unrealizedPnl': '-39.06'}, 'type': 'oneWay'},
             {'position':
-                {'coin': 'ONDO', 'cumFunding': {'allTime': '0.149334', 'sinceChange': '0.114875', 'sinceOpen': '0.114875'}, 'entryPx': '0.82524', 'leverage': {'rawUsd': '-1344.853853', 'type': 'isolated', 'value': 5}, 'liquidationPx': '0.69530237', 'marginUsed': '330.448387', 'maxLeverage': 10, 'positionValue': '1675.30224', 'returnOnEquity': '-0.01454122', 'szi': '2036.0', 'unrealizedPnl': '-4.8864'}, 'type': 'oneWay'}
-                ],
+                {'coin': 'ONDO', 'cumFunding': {'allTime': '0.149334', 'sinceChange': '0.114875', 'sinceOpen': '0.114875'}, 'entryPx': '0.82524', 
+                
+        'leverage': {'rawUsd': '-1344.853853', 'type': 'isolated', 'value': 5}, 'liquidationPx': '0.69530237', 'marginUsed': '330.448387', 'maxLeverage': 10, 'positionValue': '1675.30224', 'returnOnEquity': '-0.01454122', 'szi': '2036.0', 'unrealizedPnl': '-4.8864'}, 'type': 'oneWay'}
+        
+
             'crossMaintenanceMarginUsed': '90.221703', 'crossMarginSummary': {'accountValue': '265.547215', 'totalMarginUsed': '306.616173', 'totalNtlPos': '4329.85539', 'totalRawUsd': '-4064.308175'},
             'marginSummary': {'accountValue': '595.995602', 'totalMarginUsed': '637.06456', 'totalNtlPos': '6005.15763', 'totalRawUsd': '-5409.162028'}, 'time': 1714834365581, 'withdrawable': '0.0'}
 
