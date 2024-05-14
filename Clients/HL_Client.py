9dd539db918349e3c717481cb0683f946c014f7c
Ben Nelligan <bnelligan18@gmail.com>
2024-05-14 09:19:44 -0400

    Clients

---
+++ Clients/HL_Client.py
@@ -0,0 +1,18 @@
+from hyperliquid.exchange import exchange as ex
+from hyperliquid.info import Info
+from eth_account.account import Account
+from HL.utils import setup
+import os
+
+
+class HL_Client:
+
+    _address: Account
+    info: Info
+    exchange: ex
+
+    def __init__(self) -> None:
+        _address, info, exchange = setup()
+        self._address = _address
+        self.info = info
+        self.exchange = exchange
