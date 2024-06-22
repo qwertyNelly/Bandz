ee9111cde04f26f0fe347d2c5e4f52ca6b62a838
Ben Nelligan <36170767+qwertyNelly@users.noreply.github.com>
2024-06-22 03:58:07 -0400

    Update PriceFeed.py

--- HL/src/Strategies/PriceFeed.py
+++ HL/src/Strategies/PriceFeed.py
@@ -2,29 +2,37 @@
 import threading
 import hyperliquid.utils.types as hlt
 import hyperliquid.utils.constants as hlc
+from HL.src.Strategies.TDSequential import tds
 
-class Feed(threading.Thread):
+class BookFeed(threading.Thread):
 
 
-    coin : str 
+    coin : str
+    book : hlt.L2BookData
 
-    def __init__(self, coin : str, is_buy : bool, ):
-        self.name = coin.upper()
-        if self.name in threading.enumerate():
-            raise threading.ThreadError(f"Duplicate? Thread for {self.coin}")
-        self.lvl : hlt.List[hlt.L2Level] = None
-        self.l2book : hlt.L2BookData = None
-        self.position_side = None
+    def __init__(self, coin : str, is_buy : bool, size : int, strategy : B, n : int = 0):
+        self.n = n
+        self.size = size
+        self.name = coin.upper() # Thread Name 
         self.side = is_buy
+        self.position = position
+        self.strategy = strategy
+        
+        if self.name in threading.enumerate():
+        
+            raise threading.ThreadError(f"Duplicate? Thread for {self.coin}")
+        self.lvl : hlt.List[hlt.L2Level] = None  # type: hlt.List[hlt.L2Level]
+        self.l2book : hlt.L2BookData = None
+        
 
 
     @classmethod
-    def init_l2book(cls, price : str, size : str):
+    def init_l2book(cls, price : str, size : str, lvl : hlt.L2Level | None):
         if cls.lvl is None:
-
             cls.lvl[hlt.L2Level(str=cls.coin, sz=size, px=price, n=1)]
         if cls.l2book is None:
             # TODO: Init Book
+
 
     @classmethod
     def get_data(cls):
